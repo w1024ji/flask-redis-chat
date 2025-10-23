eea77ff5e757e3aa4dbc522f6658617cc0a71eb3
Wonji Lee <w1024ji@gmail.com>
2025-10-23 14:21:50 +0900

    Implement oauth provider for display

--- my_app/models.py
+++ my_app/models.py
@@ -5,6 +5,5 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(100), nullable=False, unique=True)
     nickname = db.Column(db.String(100), nullable=False)
-
     profile_image = db.Column(db.String(100), nullable=False, default='default.jpg')
-
+    oauth_provider = db.Column(db.String(50), nullable=False)
